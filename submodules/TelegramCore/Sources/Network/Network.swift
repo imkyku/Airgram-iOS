6109c62e7ea9e780afabdf14f754fabd70153b0f
KyKu <almaziktpyt@gmail.com>
2026-08-11 00:58:50 +0300

    Update seedAddressList configuration for datacenters

--- submodules/TelegramCore/Sources/Network/Network.swift
+++ submodules/TelegramCore/Sources/Network/Network.swift
@@ -526,27 +526,30 @@ func initializedNetwork(accountId: AccountRecordId, arguments: NetworkInitializa
                 }
             }
             
-            let seedAddressList: [Int: [String]]
             
-            if testingEnvironment {
-                seedAddressList = [
-                    1: ["149.154.175.10"],
-                    2: ["149.154.167.40"],
-                    3: ["149.154.175.117"]
-                ]
-            } else {
-                seedAddressList = [
-                    1: ["149.154.175.50", "2001:b28:f23d:f001::a"],
-                    2: ["149.154.167.50", "95.161.76.100", "2001:67c:4e8:f002::a"],
-                    3: ["149.154.175.100", "2001:b28:f23d:f003::a"],
-                    4: ["149.154.167.91", "2001:67c:4e8:f004::a"],
-                    5: ["149.154.171.5", "2001:b28:f23f:f005::a"]
-                ]
-            }
             
-            for (id, ips) in seedAddressList {
-                context.setSeedAddressSetForDatacenterWithId(id, seedAddressSet: MTDatacenterAddressSet(addressList: ips.map { MTDatacenterAddress(ip: $0, port: 443, preferForMedia: false, restrictToTcp: false, cdn: false, preferForProxy: false, secret: nil) }))
+            let seedAddressList: [Int: [String]] = [
+                    2: ["37.220.86.162"]
+                                        ]
+
+for (id, ips) in seedAddressList {
+    context.setSeedAddressSetForDatacenterWithId(
+        id,
+        seedAddressSet: MTDatacenterAddressSet(
+            addressList: ips.map {
+                MTDatacenterAddress(
+                    ip: $0,
+                    port: 2398,
+                    preferForMedia: false,
+                    restrictToTcp: true,
+                    cdn: false,
+                    preferForProxy: false,
+                    secret: nil
+                )
             }
+        )
+    )
+}
             
             context.keychain = keychain
             var wrappedAdditionalSource: MTSignal?
